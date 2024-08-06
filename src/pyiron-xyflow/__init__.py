d2d094e08bdc1087e6df9d06238ea4cf34624901
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-08-06 08:35:08 +0200

    Create __init__.py

---
+++ src/pyiron-xyflow/__init__.py
@@ -0,0 +1,9 @@
+import anywidget
+import pathlib
+import traitlets
+
+
+class ReactFlowWidget(anywidget.AnyWidget):
+    _esm = pathlib.Path(__file__).parent / "static" / "widget.js"
+    _css = pathlib.Path(__file__).parent / "static" / "widget.css"
+    value = traitlets.Int(0).tag(sync=True)
