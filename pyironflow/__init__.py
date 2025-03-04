bf7021aeb699647e1a42bbeaae07fc2195df05d8
Marvin Poul <poul@mpie.de>
2025-03-04 18:02:55 +0100

    Safeguard convenience import
    When pip install/python setup.py try to access versioneer version number
    the package may not import correctly yet, because js sources are not yet build.

--- pyironflow/__init__.py
+++ pyironflow/__init__.py
@@ -4,4 +4,9 @@ from ._version import get_versions
 # Set version of pyiron_base
 __version__ = get_versions()["version"]
 
-from .pyironflow import PyironFlow
+try:
+    from .pyironflow import PyironFlow
+except FileNotFoundError:
+    print("WARNING: could not import PyironFlow, likely because js sources "
+          "are not build and we are in build env that just tries to get the "
+          "version number.")
