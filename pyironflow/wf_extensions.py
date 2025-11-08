50d40b3650cf3fa655293a73018c909e3668fcbe
Shankha Nag <156122218+nagshankha@users.noreply.github.com>
2025-11-08 23:57:21 +0000

    changed from pyiron_workflow.channels import NotData to from pyiron_workflow.api import NotData

--- pyironflow/wf_extensions.py
+++ pyironflow/wf_extensions.py
@@ -1,5 +1,5 @@
 from pyiron_workflow.type_hinting import type_hint_to_tuple, valid_value
-from pyiron_workflow.channels import NotData
+from pyiron_workflow.api import NotData
 from pyiron_workflow.node import Node
 from pyironflow.themes import get_color
 import importlib
